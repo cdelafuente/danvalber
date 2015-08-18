931fa51d468273c3f24dcb595a119983da355761
Daniel de la Fuente <cristhian.delafuente@gmail.com>
2015-08-18 14:29:07 -0700

    Added prod task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,22 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     sass: {
-      dist: {
+      dev: {
         options: {
           style: 'compressed'
+        },
+        files: [{
+          expand: true,
+          cwd: 'styles/sass/pages',
+          src: ['*.scss'],
+          dest: 'styles/css',
+          ext: '.css'
+        }]
+      },
+      prod: {
+        options: {
+          style: 'compressed',
+          sourcemap: 'none'
         },
         files: [{
           expand: true,
